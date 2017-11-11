af9b1fb11051db36ebfb5bc750b50a0ff829c06b
Simon <simon@desktop-qurnjm1.home>
2017-11-11 16:07:21 +0100

    mise à jour

---
+++ py/averageTimeMin.py
@@ -0,0 +1,30 @@
+import sys
+
+
+def average(p_tab):
+	if len(p_tab) == 0 :
+		return ""
+	if int(sys.argv[1]) == 1 :
+		return str(p_tab[0])
+	elif int(sys.argv[1]) == 2 :
+		return str(sum(p_tab)/2)
+	elif int(sys.argv[1]) < 10 :
+		cut = 1
+	else :
+		cut = int(sys.argv[1])//10
+	l_tab = sorted(p_tab)[:cut]
+	return str(sum(l_tab)/len(l_tab))
+
+g_f = open("/Users/simon/workspace/client_ES/tmp/timeAverage", "w")
+with open('/Users/simon/workspace/client_ES/tmp/time') as c_fp:
+	l_average = []
+	for c_line in c_fp :
+		if c_line == "\n" or c_line == "" :
+			pass
+		elif c_line[0] == "#" :
+			g_f.write(average(l_average) + "\n")
+			g_f.write(c_line[:-1] + "\t:\t")
+			l_average = []
+		else :
+			l_average.append(float(c_line))
+	g_f.write(average(l_average))
--- sh/timer.sh
+++ sh/timer.sh
@@ -1,34 +1,42 @@
 #!/bin/bash
-LOOP=100
 
+LOOP=100
 echo > tmp/time
 
+IT=0
 echo \#py1000 >> tmp/time
-for i in {1..$LOOP}; do
+while [  $IT -lt $LOOP ]; do
 	# care gtime (MacOs) = time (Linux)
     (gtime --f '%e' python py/client_clone.py 1000) 2>> tmp/time
+    let IT=IT+1
 done
 
+IT=0
 echo \#pypy1000 >> tmp/time
-for i in {1..$LOOP}; do
+while [  $IT -lt $LOOP ]; do
 	# care gtime (MacOs) = time (Linux)
     (gtime --f '%e' ../pypy2-v5.9.0-osx64/bin/pypy py/client_clone.py 1000) 2>> tmp/time
+    let IT=IT+1
 done
 
 for j in {1..10}; do
 
+	IT=0
 	echo \#goB1000-$j >> tmp/time
 	go build go/client_clone.go
-	for i in {1..$LOOP}; do
+	while [  $IT -lt $LOOP ]; do
 		# care gtime (MacOs) = time (Linux)
     	(gtime --f '%e' ./client_clone 1000 $j) 2>> tmp/time
+    	let IT=IT+1
 	done
 	rm client_clone
 
+	IT=0
 	echo \#go1000-$j >> tmp/time
-	for i in {1..$LOOP}; do
+	while [  $IT -lt $LOOP ]; do
 		# care gtime (MacOs) = time (Linux)
     	(gtime --f '%e' go run go/client_clone.go 1000 $j) 2>> tmp/time
+    	let IT=IT+1
 	done
 
 done
